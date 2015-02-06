3308f0ee80d4484c2d27557a607adae2a48b5de4
NeferTiyi <nefertiyi@gmail.com>
2015-02-06 12:01:08 +0100

    remove + from % curves when plotting full range

--- tools/plot_bilan.py
+++ tools/plot_bilan.py
@@ -17,89 +17,6 @@ from matplotlib.backends.backend_pdf import PdfPages
 from gencmip6 import *
 
 
-# ########################################
-# def string_to_percent(x):
-#   """
-#   """
-#   return float(x.strip("%"))/100.
-
-
-# ########################################
-# def string_to_float(x):
-#   """
-#   """
-#   return float(x.strip("h"))
-
-
-# ########################################
-# def string_to_date(ssaammjj, fmt="%Y-%m-%d"):
-#   """
-#   """
-#   return dt.datetime.strptime(ssaammjj, fmt)
-
-
-# ########################################
-# def date_to_string(dtdate, fmt="%Y-%m-%d"):
-#   """
-#   """
-#   return dt.datetime.strftime(dtdate, fmt)
-
-
-# ########################################
-# def get_last_file(dir_data, pattern):
-#   """
-#   """
-#   current_dir = os.getcwd()
-#   os.chdir(dir_data)
-#   filename = pattern + "*"
-#   return_value = sorted(glob.glob(os.path.join(dir_data, filename)))[-1]
-#   os.chdir(current_dir)
-#   return return_value
-
-
-# ########################################
-# class Project(object):
-
-#   #---------------------------------------
-#   def __init__(self):
-#     self.project   = ""
-#     self.date_init = ""
-#     self.deadline  = ""
-#     self.alloc     = 0
-
-#   #---------------------------------------
-#   def fill_data(self, filein):
-#     import json
-#     dico = json.load(open(filein, "r"))
-#     self.project = dico["project"]
-#     self.deadline = string_to_date(dico["deadline"]) + \
-#                     dt.timedelta(days=-1)
-#     self.alloc = dico["alloc"]
-
-#   #---------------------------------------
-#   def get_date_init(self, filein):
-#     data = np.genfromtxt(
-#       filein,
-#       skip_header=1,
-#       converters={0: string_to_date,
-#                   1: string_to_percent},
-#       missing_values="nan",
-#     )
-#     dates, utheos = zip(*data)
-
-#     (x1, x2) = (np.nanargmin(utheos), np.nanargmax(utheos))
-
-#     m = np.array([[x1, 1.], [x2, 1.]])
-#     n = np.array([utheos[x1], utheos[x2]])
-
-#     (a, b) = np.linalg.solve(m, n)
-
-#     delta = int(round((-b/a)-x1 + 1))
-
-#     d1 = dates[x1]
-#     self.date_init = d1 + dt.timedelta(days=delta)
-
-
 ########################################
 class BilanDict(dict):
   #---------------------------------------
@@ -248,17 +165,22 @@ def plot_data(ax_conso, ax_theo, xcoord, xlabels,
               consos, theo_uses, real_uses, theo_equs):
   """
   """
+  if args.full:
+    line_style = "-"
+  else:
+    line_style = "+-"
+
   ax_conso.bar(xcoord, consos, align="center", color="linen",
                linewidth=0.2, label="conso (heures)")
 
   ax_theo.plot(xcoord, theo_equs, "--",
                color="firebrick", linewidth=0.5,
                solid_capstyle="round", solid_joinstyle="round")
-  ax_theo.plot(xcoord, theo_uses, "+-", color="firebrick",
+  ax_theo.plot(xcoord, theo_uses, line_style, color="firebrick",
                linewidth=1, markersize=8,
                # solid_capstyle="round", solid_joinstyle="round",
                label="conso théorique (%)")
-  ax_theo.plot(xcoord, real_uses, "+-", color="forestgreen",
+  ax_theo.plot(xcoord, real_uses, line_style, color="forestgreen",
                linewidth=1, markersize=8,
                # solid_capstyle="round", solid_joinstyle="round",
                label="conso réelle (%)")
